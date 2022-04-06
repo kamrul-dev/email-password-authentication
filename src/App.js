f53f45a8cef65f04dfd82053bb93be79aff61836
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-07 02:05:41 +0600

    Initialize Firebase Authentication and get a reference to the service

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import getAuth from 'firebase/auth';
+import app from './firebase.init';
+
+
+const auth = getAuth(app)
 
 function App() {
   return (
