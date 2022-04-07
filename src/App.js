cac20cbe98115e94ed43212deaf16c95231e0c8b
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-07 11:12:32 +0600

    Login form validation with regular required method

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { useState } from 'react';
 const auth = getAuth(app);
 
 function App() {
-
+  const [validated, setValidated] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -22,14 +22,27 @@ function App() {
   }
 
   const handleFormSubmit = event => {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then(result => {
-        const user = result.user;
-        console.log(user);
-      })
-      .catch(error => {
-        console.error(error);
-      })
+
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      // return;
+    }
+
+    setValidated(true);
+    if (form.checkValidity() === true) {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then(result => {
+          const user = result.user;
+          console.log(user);
+        })
+        .catch(error => {
+          console.error(error);
+        })
+    }
+
+
     event.preventDefault();
   }
 
@@ -37,18 +50,24 @@ function App() {
     <div>
       <div className="registration w-50 mx-auto mt-5">
         <h2 className='text-info'> Please Register Yourself!</h2>
-        <Form onSubmit={handleFormSubmit}>
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid email!
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
+            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid password !
+            </Form.Control.Feedback>
           </Form.Group>
           <Button variant="info" type="submit">
             Submit
